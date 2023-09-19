8ddd53ad5d556d5b5941440221c513f95f7be136
Yau1heni <lodbrok9@gmail.com>
2023-09-19 20:40:44 +0300

    feat: Routing, Lazy and Suspense

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,5 +5,6 @@ export function buildDevServer(options: BuildOptionsType): DevServerConfiguratio
   return {
     port: options.port,
     open: true,
+    historyApiFallback: true,
   }
 }
--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,21 @@
-import {Counter} from './component/counter';
-import './index.scss'
+import './index.scss';
+import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPageAsync} from './pages/about-page/about-page.async';
+import {MainPageAsync} from './pages/main-page/main-page.async';
+import {Suspense} from 'react';
 
 export const App = () => {
   return (
-    <div className='app'>
-      asdasasd
-      <Counter/>
+    <div className="app">
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync/>}/>
+          <Route path={'/'} element={<MainPageAsync/>}/>
+          <Route/>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
