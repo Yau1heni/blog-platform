7274f604a940debf515de23c5ab5b34936105ebd
Yau1heni <lodbrok9@gmail.com>
2023-09-27 19:17:31 +0300

    feat: HotModuleReplacementPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -16,6 +16,8 @@ export function buildPlugins({paths, isDev}: BuildOptionsType): webpack.WebpackP
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    })
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
   ]
 }
