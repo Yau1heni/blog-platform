96841481949ae84aae277f6b0f72d7aad2899667
Yau1heni <lodbrok9@gmail.com>
2023-10-27 20:28:53 +0300

    feat: article types

---
+++ src/entities/article/model/types/article.ts
@@ -0,0 +1,45 @@
+export enum ArticleBlockType {
+  CODE = 'CODE',
+  IMAGE = 'IMAGE',
+  TEXT = 'TEXT',
+}
+
+export type ArticleBlockBase = {
+  id: string;
+}
+
+export type ArticleCodeBlock = {
+  type: ArticleBlockType.CODE;
+  code: string;
+} & ArticleBlockBase
+
+export type ArticleImageBlock = {
+  type: ArticleBlockType.IMAGE;
+  src: string;
+  title: string;
+} & ArticleBlockBase
+
+export type ArticleTextBlock = {
+  type: ArticleBlockType.TEXT;
+  paragraphs: string[];
+  title?: string;
+} & ArticleBlockBase
+
+export type ArticleBlock = ArticleCodeBlock | ArticleImageBlock | ArticleTextBlock;
+
+export enum ArticleType {
+  IT = 'IT',
+  SCIENCE = 'SCIENCE',
+  ECONOMICS = 'ECONOMICS'
+}
+
+export type Article = {
+  id: string;
+  title: string;
+  subtitle: string;
+  img: string;
+  views: number;
+  createdAt: string;
+  type: ArticleType[];
+  blocks: ArticleBlock[];
+}
